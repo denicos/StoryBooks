9b3e80975719b1ccd1a41191ece199c20e2b2cd4
denicos <nicoswaves.mn@gmail.com>
2022-06-27 13:40:23 +0300

    initial commit
    ....

---
+++ app.js
@@ -0,0 +1,35 @@
+const path = require('path')
+const express = require('express')
+const dotenv = require('dotenv')
+const morgan = require('morgan')
+const exphbs = require('express-handlebars')
+const connectDB = require('./config/db')
+
+
+//load config
+dotenv.config({ path : './config/config.env'})
+
+connectDB()
+const app = express()
+
+//logging
+if(process.env.NODE_ENV === 'development'){
+    app.use(morgan('dev'))
+}
+
+
+
+//handlebars
+app.engine('.hbs', exphbs.engine({ defaultLayout: 'main',extname : '.hbs'}));
+app.set('view engine', '.hbs');
+
+//static files
+app.use(express.static(path.join(__dirname, 'public')))
+
+//routes
+app.use('/', require('./routes/index'))
+
+const PORT = process.env.PORT || 3000
+
+app.listen(PORT,
+     console.log(`server runnin in ${process.env.NODE_ENV} mode on port ${PORT}`))

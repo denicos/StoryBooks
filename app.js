982cf04dd73e9ecacf29ce4922086093296857d3
denicos <nicoswaves.mn@gmail.com>
2022-07-04 18:16:56 +0300

    add stories of different users

--- app.js
+++ app.js
@@ -1,33 +1,68 @@
 const path = require('path')
 const express = require('express')
+const mongoose = require('mongoose')
 const dotenv = require('dotenv')
 const morgan = require('morgan')
 const exphbs = require('express-handlebars')
+const passport = require('passport')
+const session = require('express-session')
+const MongoStore = require('connect-mongo')
 const connectDB = require('./config/db')
 
 
 //load config
 dotenv.config({ path : './config/config.env'})
 
+//passport config
+require('./config/passport')(passport)
+
 connectDB()
 const app = express()
+
+//body parser
+app.use(express.urlencoded({extended : false}))
+app.use(express.json())
 
 //logging
 if(process.env.NODE_ENV === 'development'){
     app.use(morgan('dev'))
 }
 
+//handlebars helpers
+const { formatDate } = require('./helpers/hbs')
 
 
 //handlebars
-app.engine('.hbs', exphbs.engine({ defaultLayout: 'main',extname : '.hbs'}));
+app.engine('.hbs', exphbs.engine({  helpers : {
+  formatDate
+} ,defaultLayout: 'main',extname : '.hbs'}));
 app.set('view engine', '.hbs');
+
+//sessions
+app.use(session({
+    secret: 'keyboard cat',
+    resave: false,
+    saveUninitialized: false,
+    store :MongoStore.create({
+      mongoUrl : process.env.MONGO_URI
+    })
+  }))
+
+
+//passport middle ware 
+app.use(passport.initialize())
+app.use(passport.session())
+
+
 
 //static files
 app.use(express.static(path.join(__dirname, 'public')))
 
 //routes
 app.use('/', require('./routes/index'))
+app.use('/auth', require('./routes/auth'))
+app.use('/stories', require('./routes/stories'))
+
 
 const PORT = process.env.PORT || 3000
 
---
+++ routes/stories.js
@@ -0,0 +1,27 @@
+const express = require('express')
+const router = express.Router()
+const { ensureAuth } = require('../middleware/auth')
+
+const Story = require('../models/Story')
+
+//@desc show add page
+//@route GET /stories/add
+router.get('/add',  ensureAuth, (req, res) => {
+    res.render('stories/add')
+})
+
+
+//@desc process add story form
+//@route post /stories
+router.post('/',  ensureAuth,  async (req, res) => {
+    try {
+        req.body.user = req.user.id
+        await Story.create(req.body)
+        res.redirect('/dashboard')
+    } catch (err) {
+        console.error(err)
+        res.render('errors/500 ')
+        
+    }
+})
+module.exports = router

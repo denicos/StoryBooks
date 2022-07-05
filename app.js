031f981560cbcd7890d91183aac7297042ffb4e8
denicos <nicoswaves.mn@gmail.com>
2022-07-05 15:59:37 +0300

    done_for_now

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const mongoose = require('mongoose')
 const dotenv = require('dotenv')
 const morgan = require('morgan')
 const exphbs = require('express-handlebars')
+const methodOverride = require('method-override')
 const passport = require('passport')
 const session = require('express-session')
 const MongoStore = require('connect-mongo')
@@ -23,18 +24,32 @@ const app = express()
 app.use(express.urlencoded({extended : false}))
 app.use(express.json())
 
+//put method override
+app.use(methodOverride(function (req, res) {
+  if (req.body && typeof req.body === 'object' && '_method' in req.body) {
+    // look in urlencoded POST bodies and delete it
+    let method = req.body._method
+    delete req.body._method
+    return method
+  }
+}))
+
 //logging
 if(process.env.NODE_ENV === 'development'){
     app.use(morgan('dev'))
 }
 
 //handlebars helpers
-const { formatDate } = require('./helpers/hbs')
+const { formatDate, stripTags, truncate, editIcon, select } = require('./helpers/hbs')
 
 
 //handlebars
 app.engine('.hbs', exphbs.engine({  helpers : {
-  formatDate
+  formatDate,
+  stripTags,
+  truncate,
+  editIcon,
+  select,
 } ,defaultLayout: 'main',extname : '.hbs'}));
 app.set('view engine', '.hbs');
 
@@ -52,6 +67,12 @@ app.use(session({
 //passport middle ware 
 app.use(passport.initialize())
 app.use(passport.session())
+
+//set global variable.
+app.use(function(req, res, next){
+  res.locals.user = req.user || null
+  next()
+})
 
 
 
